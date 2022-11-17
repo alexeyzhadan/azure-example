30364745e7eef263ac0aa592ca3e95c5194ce410
Alexey Zhadan <alexey.zhadan@ukad-group.com>
2022-11-17 17:23:03 +0200

    Added project with a test azure function

---
+++ AzureExample.AzureFunction/HttpTriggerExampleFunction.cs
@@ -0,0 +1,34 @@
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace AzureExample.AzureFunction
+{
+    public static class HttpTriggerExampleFunction
+    {
+        [FunctionName("HttpTriggerExampleFunction")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest request,
+            ILogger logger)
+        {
+            logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = request.Query["name"];
+
+            string requestBody = await new StreamReader(request.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
